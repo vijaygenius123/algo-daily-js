4db19efa4dbf9bfc8fd5673804685d8d4c87bc5c
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-07-07 18:54:37 +0100

    Two Sum Typos

--- src/day_21-30/day_25/index.js
+++ src/day_21-30/day_25/index.js
@@ -1,14 +1,14 @@
 function twoSum(arr, goal) {
     const results = [];
-    const hash = {}
+    const hash = {};
 
     for(let idx in arr){
-        const difference = goal - arr[idx]
+        const difference = goal - arr[idx];
         if(Object.hasOwn(hash, arr[idx])){
-            results[0] = parseInt(hash[arr[idx]])
-            results[1] = parseInt(idx)
+            results[0] = parseInt(hash[arr[idx]]);
+            results[1] = parseInt(idx);
         } else {
-            hash[difference] = idx
+            hash[difference] = idx;
         }
     }
     return results;
